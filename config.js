13b5a13fb226211f01e664aa0d8fcb8e2f733ec9
DeYing <ducanh18299@gmail.com>
2021-11-09 22:15:19 +0700

    upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Châm à!',
+    introDesc: `Anh yêu em nhiều nắm....`,
+    btnIntro: '❤',
+    title: 'Nếu cuộc đời là một bức tranh',
+    desc: 'Vậy thì mình cùng nhau vẽ một bức tranh thật đẹp nhé 😍😍😍',
+    btnYes: 'Dạaaaaaaaaa ❤❤',
+    btnNo: 'Không đâu 😶',
+    question:'Em có muốn mời anh làm chú rể trong đám cưới của em không 😎',
+    btnReply: 'Send',
+    reply: 'Dạ muốn ạaaaaa ❤❤❤',
+    mess: 'Yêu em nhiềuuuu 😘😘',
+    messDesc: 'Nhắn cho anh những lời yêu thương nhé 😙',
+    btnAccept: 'Okiiiii ❤',
+    messLink: 'https://www.messenger.com/t/100007291034608/' 
+}
